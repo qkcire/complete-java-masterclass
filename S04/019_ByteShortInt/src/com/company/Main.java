7f8178ae2eea715e13bdeb5906c8b4d37e8e02ec
Erick Quintanilla <erickbquintanilla@gmail.com>
2017-06-12 21:12:39 -0700

    S04 L19 - Primitive Types

--- S04/019_ByteShortInt/src/com/company/Main.java
+++ S04/019_ByteShortInt/src/com/company/Main.java
@@ -4,21 +4,22 @@ public class Main {
 
     public static void main(String[] args) {
 
-//        // int has a width of 32
-//        int myMinValue = -2_147_483_648; //
-//        int myMaxValue =  2_147_483_647;
-//        int myTotal = (myMinValue/2);
-//        System.out.println("MyTotal = " + myTotal);
-//
-//        // int has a width of 8
-//        byte myByteValue = -128; // largest is 127
-//        byte myNewByteValue = (byte)(myByteValue/2); // type-casting because it treats it as an int
-//        System.out.println("myNewBytevalue = " + myNewByteValue);
-//        // int has a width of 16
-//        short myShortValue = -32_768; // largest = 32_767
-//        short myNewShortValue = (short) (myShortValue/2);
-//        // long has a width of 64
-//        long myLongValue = -9_223_372_036_854_775_807L;
+        // int has a width of 32
+        int myMinValue = -2_147_483_648; //
+        int myMaxValue =  2_147_483_647;
+        int myTotal = (myMinValue/2);
+        System.out.println("MyTotal = " + myTotal);
+
+        // int has a width of 8
+        byte myByteValue = -128; // largest is 127
+        byte myNewByteValue = (byte)(myByteValue/2); // type-casting because it treats it as an int
+        System.out.println("myNewBytevalue = " + myNewByteValue);
+        // int has a width of 16
+        short myShortValue = -32_768; // largest = 32_767
+        short myNewShortValue = (short) (myShortValue/2);
+        // long has a width of 64
+        long myLongValue = -9_223_372_036_854_775_807L;
+
         byte tByte = 13;
         short tShort = 1312;
         int tInt = 3;
