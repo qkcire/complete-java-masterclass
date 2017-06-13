f43a1076ea8beba415159b9755fbdd4c97cb6160
Erick Quintanilla <erickbquintanilla@gmail.com>
2017-06-12 19:46:23 -0700

    First Java Program

---
+++ S03/015_HelloWorld/src/Hello.java
@@ -0,0 +1,9 @@
+/**
+ * Created by eq on 6/12/17.
+ */
+public class Hello {
+    public static void main(String[] args){
+        //System.out.println("Hello, World!");
+        System.out.println("Hello, Erick!");
+    }
+}
