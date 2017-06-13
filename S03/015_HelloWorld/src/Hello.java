29f7361cee6040cc6825b9e615b154a2add2757e
Erick Quintanilla <erickbquintanilla@gmail.com>
2017-06-12 20:24:51 -0700

    S04 L18

--- S03/015_HelloWorld/src/Hello.java
+++ S03/015_HelloWorld/src/Hello.java
@@ -2,8 +2,14 @@
  * Created by eq on 6/12/17.
  */
 public class Hello {
-    public static void main(String[] args){
-        //System.out.println("Hello, World!");
+    public static void main(String[] args) {
         System.out.println("Hello, Erick!");
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        int myNewTotal = 1000 - myTotal;
+        System.out.println(myNewTotal);
+
     }
 }
