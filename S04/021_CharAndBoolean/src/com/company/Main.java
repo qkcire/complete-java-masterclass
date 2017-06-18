c69f2c440d8523a59f26192f924377c710ba6bb8
Erick Quintanilla <erickbquintanilla@gmail.com>
2017-06-18 13:57:11 -0700

    Char and Boolean intro

---
+++ S04/021_CharAndBoolean/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        char myChar = '\u00A9';
+        System.out.println("Unicode output was: " + myChar);
+
+        boolean myBoolean = true;
+        boolean isMale = true;
+
+        // CHALLENGE
+        char myRegistered = '\u00AE';
+        System.out.println("Unicode output was: " + myRegistered);
+    }
+}
