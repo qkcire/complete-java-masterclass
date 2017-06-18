af32afac43604e15d6032d3603eab28e1a354eda
Erick Quintanilla <erickbquintanilla@gmail.com>
2017-06-18 13:31:36 -0700

    Float and Double practice

---
+++ S04/020_FloatAndDouble/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int myIntValue = 5;
+        float myFloatValue = 5f;
+        double myDoubleValue = 5d;
+        System.out.println("My int value = " + myIntValue);
+        System.out.println("My float value = " + myFloatValue);
+        System.out.println("My double value = " + myDoubleValue);
+    }
+}
