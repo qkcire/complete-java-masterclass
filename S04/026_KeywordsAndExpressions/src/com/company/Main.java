ec31a6d160b93cdc86af903f820660a110c1d337
Erick Quintanilla <erickbquintanilla@gmail.com>
2017-06-19 22:13:36 -0700

    Keywords and Expressions intro

---
+++ S04/026_KeywordsAndExpressions/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // a mile is equal to 1.609344 kilometres
+        // an expression
+        double kilometres = (100 * 1.609344);
+        int highScore = 50;
+
+        // an expression
+        if (highScore == 50) {
+            System.out.println("This is an expression.");
+        }
+    }
+}
