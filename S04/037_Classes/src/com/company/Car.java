8bc1ee6fc0ea3c2e8aa14b8d6d340a1b68eb4125
Erick Quintanilla <erickbquintanilla@gmail.com>
2017-06-21 20:26:53 -0700

    Class intro + Car class

---
+++ S04/037_Classes/src/com/company/Car.java
@@ -0,0 +1,28 @@
+package com.company;
+
+/**
+ * Created by eq on 6/21/17.
+ */
+public class Car {
+
+    private int doors;
+    private int wheels;
+    private String model;
+    private String engine;
+    private String color;
+
+    public void setModel(String model) {
+        String validModel = model.toLowerCase();
+        if (validModel.equals("carrera") || validModel.equals("commodore")) {
+            this.model = model;
+        } else {
+            this.model = "unknown";
+        }
+
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+}
+
