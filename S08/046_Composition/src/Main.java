a6a3b1acd26eb82f7fbcc81944ebbccbce5c26f8
Erick Quintanilla <erickbquintanilla@gmail.com>
2017-06-23 14:15:19 -0700

    Comment out PC composition example

--- S08/046_Composition/src/Main.java
+++ S08/046_Composition/src/Main.java
@@ -1,15 +1,19 @@
 public class Main {
-
+    // intro to composition vs inheritance
+    // when building a program, always look at building a composed structure first
+    // rather than a inherited structure.
     public static void main(String[] args) {
-        Dimensions dimensions = new Dimensions(20, 20, 5);
-	    Case theCase = new Case("220B", "Dell", "240", dimensions);
-        Monitor theMonitor = new Monitor("27inch Beast", "Acer", 27, new Resolution(2540, 1440));
-        Motherboard theMotherboard = new Motherboard("BJ-200", "Asus", 4, 6, "v2.44");
+//        Dimensions dimensions = new Dimensions(20, 20, 5);
+//	      Case theCase = new Case("220B", "Dell", "240", dimensions);
+//        Monitor theMonitor = new Monitor("27inch Beast", "Acer", 27, new Resolution(2540, 1440));
+//        Motherboard theMotherboard = new Motherboard("BJ-200", "Asus", 4, 6, "v2.44");
+//
+//        PC thePC = new PC(theCase, theMonitor, theMotherboard);
+//        thePC.powerUp();
 
-        PC thePC = new PC(theCase, theMonitor, theMotherboard);
+        Room masterBedroom = new Room("California King", 3, true);
+        House grandmasHouse = new House(masterBedroom);
+        grandmasHouse.tVCheck();
 
-        thePC.getMonitor().drawPixelAt(1500, 1200, "red");
-        thePC.getMotherboard().loadProgram("Windows 1.0");
-        thePC.getTheCase().pressPowerButton();
     }
 }
