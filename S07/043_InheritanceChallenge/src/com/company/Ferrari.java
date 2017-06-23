d00f9e8251328312e2235416803398e82cda6a15
Erick Quintanilla <erickbquintanilla@gmail.com>
2017-06-23 12:02:47 -0700

    Ferrari class

---
+++ S07/043_InheritanceChallenge/src/com/company/Ferrari.java
@@ -0,0 +1,17 @@
+package com.company;
+
+/**
+ * Created by eq on 6/23/17.
+ */
+public class Ferrari extends Car{
+    private String trim;
+
+    public Ferrari(int gears, String trim) {
+        super("Ferrari", "GTC4Lusso", "white", 2017, 350, gears);
+        this.trim = trim;
+    }
+
+    public String getTrim() {
+        return trim;
+    }
+}
